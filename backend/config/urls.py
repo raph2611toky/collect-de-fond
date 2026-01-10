6e309ac339285d3a3a9ac0977cc6c487c8f5ecac
raph2611toky <raphaeltokinandrasana@gmail.com>
2026-01-10 14:25:20 +0300

    commit backend user

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,35 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from .views import swagger_password_protect
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Collecte de Fond",
+        default_version='v1',
+        description="API pour Collecte de Fond",
+        terms_of_service="#",
+        contact=openapi.Contact(email="contact@monapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny]
+)
+
+protected_swagger = swagger_password_protect(schema_view.with_ui('swagger', cache_timeout=0))
+protected_redoc = swagger_password_protect(schema_view.with_ui('redoc', cache_timeout=0))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/admin/', include('apps.users.urls')),
+    
+    path('api/docs/', protected_swagger, name='schema-swagger-ui'),
+    path('api/redoc/', protected_redoc, name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ backend/helpers/constantes.py
@@ -0,0 +1,8 @@
+TEMPLATES_EMAIL = {
+    "verify_email":"emails/otp_verification.html",
+    "reinitialiser_mot_de_passe":"emails/reset_password.html",
+    'support_message': 'emails/support_message.html',
+    'employe_set_password': 'emails/employe_set_password.html',
+    'employe_confirmation': 'emails/employe_confirmation.html',
+    'employe_reset_password': 'emails/employe_reset_password.html',
+}
